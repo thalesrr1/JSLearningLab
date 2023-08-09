e5b96eab0f39ae8daa0d9af8483b9f76a630504c
Thales <thalestavares.r@gmail.com>
2023-08-09 09:03:33 -0300

    testing object destructuring assignment

--- aula33/script.js
+++ aula33/script.js
@@ -1,9 +1,19 @@
 const pessoa = {
     nome: 'Thales',
-    sobrenome:'Tavares';
+    sobrenome:'Tavares',
     idade: 30,
     endereco: {
         rua: 'Av JK',
         numero
     }
 };
+
+
+// Atribuição via desestruturação
+//posso redeclarar o nome da variavel utilizando por ex nome : n
+// podemos especificar um valor padrao a variavel nome = ''
+const {nome: n = '', sobrenome } = pessoa;
+console.log(nome, sobrenome);
+
+const { endereco: {rua, numero}, endereco} = pessoa;
+console.log(rua, numero, endereco);
