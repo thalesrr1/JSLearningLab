6f72f54c311d62ed383b81b3a423fbcdece524ae
Thales <thalestavares.r@gmail.com>
2023-08-09 00:49:45 -0300

    Added array destructuring assignment

---
+++ aula32/index.js
@@ -0,0 +1,15 @@
+
+//               0    1    2    3    4    ...
+const numeros = [100, 200, 300, 400, 500, 600, 700, 800, 900];
+const [um, dois, tres, ...resto] = numeros;
+//         operador    ...rest  (pegar o resto de algo)
+//o operador spread tem o intuito de espalhar mas fazem a mesma coisa
+console.log(um, dois, tres);
+
+console.log(resto);
+
+
+//               0    1    2    3    4    ...
+const Number = [100, 200, 300, 400, 500, 600, 700, 800, 900];
+const [um, , tres, ,cinco, ,sete] = Number;
+console.log(um, tres, cinco);
