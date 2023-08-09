b0db05680eea2a3624dbb4470802c7740290ac9c
Thales <thalestavares.r@gmail.com>
2023-08-09 08:52:53 -0300

    Studying object destructuring assignment in JS

--- aula32/index.js
+++ aula32/index.js
@@ -11,5 +11,5 @@ console.log(resto);
 
 //               0    1    2    3    4    ...
 const Number = [100, 200, 300, 400, 500, 600, 700, 800, 900];
-const [um, , tres, ,cinco, ,sete] = Number;
-console.log(um, tres, cinco);
+const [one, , three, ,five, ,seven] = Number;
+console.log(one, three, five);
---
+++ aula33/script.js
@@ -0,0 +1,9 @@
+const pessoa = {
+    nome: 'Thales',
+    sobrenome:'Tavares';
+    idade: 30,
+    endereco: {
+        rua: 'Av JK',
+        numero
+    }
+};
