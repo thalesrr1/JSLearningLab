d0987b0d440a732d5af1fc9f0b48734b48165155
Thales <thalestavares.r@gmail.com>
2023-11-07 11:52:38 -0300

    fix paste location

--- aula33/script.js
+++ aula33/script.js
@@ -4,7 +4,7 @@ const pessoa = {
     idade: 30,
     endereco: {
         rua: 'Av JK',
-        numero
+        numero:  320
     }
 };
 
@@ -12,7 +12,7 @@ const pessoa = {
 // Atribuição via desestruturação
 //posso redeclarar o nome da variavel utilizando por ex nome : n
 // podemos especificar um valor padrao a variavel nome = ''
-const {nome: n = '', sobrenome } = pessoa;
+const {nome, sobrenome } = pessoa;
 console.log(nome, sobrenome);
 
 const { endereco: {rua, numero}, endereco} = pessoa;
