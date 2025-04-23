ee4a6dd1ce38eb7e62600066009751823bfc7c20
Thales <thalesroberto.f@hotmail.com>
2025-04-23 00:14:31 -0300

    chore:  add clearDisplay function in main.js

--- calc/assets/js/main.js
+++ calc/assets/js/main.js
@@ -1,6 +1,37 @@
 function criaCalculadora() {
   return {
-    
+    display: document.querySelector('.display'),
+    btnClear: document.querySelector('.btn-clear'),
+
+    clearDisplay() {
+      this.display.value = ''
+    },
+
+    inicia() {
+      this.cliqueBotoes()
+    },
+
+    cliqueBotoes() {
+      // this -> calculadora
+      document.addEventListener('click', e => {
+        const el = e.target
+        console.log(el)
+
+        if(el.classList.contains('btn-num')) {
+          this.btnToDisplay(el.innerText);
+        }
+        if(el.classList.contains('btn-clear')){
+          console.log('cheguei aqui')
+          this.clearDisplay()
+        }
+        
+      });
+    },
+
+    btnToDisplay(valor) {
+      this.display.value += valor;
+    }
+
   };
 }
 
